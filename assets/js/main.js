b651bffa59c770a164ed52ec25590945aa26a211
gicunho <gicunho@hotmail.it>
2021-04-27 10:59:55 +0200

    Fix Formula

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,11 @@ var parola_2 = prompt('Inserisci la seconda parola');
 
 //Il software stampa prima la parola più corta, poi la parola più lunga.
 if (parola_1.length > parola_2.length){
-    console.log(parola_1);
     console.log(parola_2);
+    console.log(parola_1);
 } else if (parola_2.length > parola_1.length){
-    console.log(parola_2);
     console.log(parola_1);
+    console.log(parola_2);
 } else if (parola_1.length == parola_2.length){
     console.log(parola_1);
     console.log(parola_2);
