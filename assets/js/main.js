55838e23932d8f37fb82f61b0ab8a78d197c22ed
gicunho <gicunho@hotmail.it>
2021-04-27 10:54:56 +0200

    Creato fromula per parola più lunga

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+//L’utente inserisce due parole in successione, con due prompt.
+var parola_1 = prompt('Inserisci la prima parola');
+var parola_2 = prompt('Inserisci la seconda parola');
+
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+if (parola_1.length > parola_2.length){
+    console.log(parola_1);
+    console.log(parola_2);
+} else if (parola_2.length > parola_1.length){
+    console.log(parola_2);
+    console.log(parola_1);
+} else if (parola_1.length == parola_2.length){
+    console.log(parola_1);
+    console.log(parola_2);
+}
