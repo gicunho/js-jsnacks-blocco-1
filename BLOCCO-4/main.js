b8be2a0dd0a5b333c9873bf9d1cefc549448544d
gicunho <gicunho@hotmail.it>
2021-05-10 11:49:31 +0200

    Aggiunto ciclo for in per snack 1b

--- BLOCCO-4/main.js
+++ BLOCCO-4/main.js
@@ -1,4 +1,7 @@
-/********  SNACK 1a ********/
+/********************  
+ SNACK 1a 
+ *******************/
+
 //Creare un oggetto palla che abbia le seguenti proprietà.
 //Nome = palla
 //Peso = 10
@@ -8,8 +11,18 @@ var palla = {
     peso: 10,
 }
 console.log(palla.peso);
+
+
+
+/********************  
+ SNACK 1b 
+ *******************/
+
 //Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
 
 palla.peso = Number(prompt('Inserisci il pesso della palla'));
 
-console.log(palla.peso);
+//console.log(palla.peso);
+for(key in palla){
+    console.log(key, palla[key]);
+}
