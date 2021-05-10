2a677c8a0d54a11dd907c339aa7b0db38168c5fe
gicunho <gicunho@hotmail.it>
2021-05-10 12:20:01 +0200

    Risolto JSnack 2

--- BLOCCO-4/main.js
+++ BLOCCO-4/main.js
@@ -6,13 +6,13 @@
 //Nome = palla
 //Peso = 10
 
-var palla = {
+/* var palla = {
     nome: 'palla',
     peso: 10,
 }
 console.log(palla.peso);
 
-
+ */
 
 /********************  
  SNACK 1b 
@@ -20,9 +20,32 @@ console.log(palla.peso);
 
 //Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
 
-palla.peso = Number(prompt('Inserisci il pesso della palla'));
+/* palla.peso = Number(prompt('Inserisci il pesso della palla'));
 
 //console.log(palla.peso);
 for(key in palla){
     console.log(key, palla[key]);
-}
+} */
+
+/********************  
+ SNACK 2 
+ *******************/
+
+ //Creare un oggetto che rappresenti un triangolo rettangolo, con le seguenti proprietà: base
+ 
+ 
+ var triangolo = {
+     base: 10,
+     altezza: 7,
+    }
+    
+//Calcolare perimetro e area.
+
+var ipotenusa = Math.sqrt(Math.pow(triangolo.base, 2) + Math.pow(triangolo.altezza, 2));
+console.log(ipotenusa);
+
+var perimetro = triangolo.base + triangolo.altezza + ipotenusa;
+console.log(perimetro);
+
+var area = (ipotenusa * triangolo.altezza) / 2;
+console.log(area);
