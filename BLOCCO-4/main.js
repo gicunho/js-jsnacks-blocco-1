4c8bbe48f8a64f44446dd8e8c03ffa5c5d3f29bd
gicunho <gicunho@hotmail.it>
2021-05-10 11:45:12 +0200

    Creato snack 2

--- BLOCCO-4/main.js
+++ BLOCCO-4/main.js
@@ -1,9 +1,15 @@
-/* SNACK 1 */
+/********  SNACK 1a ********/
 //Creare un oggetto palla che abbia le seguenti proprietà.
 //Nome = palla
 //Peso = 10
 
 var palla = {
-    nome: palla,
+    nome: 'palla',
     peso: 10,
 }
+console.log(palla.peso);
+//Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla.
+
+palla.peso = Number(prompt('Inserisci il pesso della palla'));
+
+console.log(palla.peso);
